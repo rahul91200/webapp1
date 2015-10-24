a4449313db14daf2c6e981cc8cc5f2b6e7011dac
rahul91200 <rahul91200@gmail.com>
2015-10-24 11:35:51 +0000

    first checkin

---
+++ app.py
@@ -0,0 +1,38 @@
+from bottle import route, run
+
+header = '''
+<html !document>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js" ></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<body>
+<div class="well">
+My Web APP
+</div>
+'''
+
+footer = """
+</body>
+</html>
+"""
+
+navs = '''
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+'''
+
+@route('/hello')
+def hello():
+    return header + navs + "<h1>Hello World!</h1>" + footer
+
+run(host='0.0.0.0', port=8080, debug=True)
